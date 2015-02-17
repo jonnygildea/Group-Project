e07ecbecd38c9f723bdbf7941348679380165be0
tzay <sidth4r@gmail.com>
2015-02-17 11:08:07 +0000

    new print statement

--- ProjectA/src/com/GitTest.java
+++ ProjectA/src/com/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First change...");
-
+		System.out.println("2nd change...");
 	}
 
 }
